f7ddc1a1637b1a3b5640480949d6c27c0223a28d
최지원 <popo2022@naver.com>
2021-08-03 01:28:47 +0900

    feat : making tool
    feat : making tool

--- Photon-Firebase/Assets/Scripts/Player/PlayerFireSolo.cs
+++ Photon-Firebase/Assets/Scripts/Player/PlayerFireSolo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f31e61fdc9cdbca08d580b839fbc57765bea43befa0845a7dfb67e272a342e3
-size 4062
+oid sha256:a0cf34ee177e52b4416f67adbd8faed5abc76d2936c16151daf7c39248f4aa5d
+size 3500
--- Photon-Firebase/Assets/Scripts/Player/SearchingItem.cs
+++ Photon-Firebase/Assets/Scripts/Player/SearchingItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00aaf39a9aaf528a8fb2225189befe5d0a3163fe11d533b8b28a27dd33e449aa
-size 1731
+oid sha256:f6f5f1217c3a016b903339d3304802201228bed84b0f230e82b1f07801e7ce41
+size 2877
