2c68e5a8314cf6a935aa38136c396e8bbad76b61
최지원 <popo2022@naver.com>
2021-08-03 23:47:21 +0900

    feat : tool
    숫자야구 알고리즘 적용, 완료
    중복 제외하고, 제거 vfx sfx만 하면 될듯

--- Photon-Firebase/Assets/Scripts/ToolData.cs
+++ Photon-Firebase/Assets/Scripts/ToolData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b361db04648aae66265d114a5ed56df153882f0ec1d292bc84fd85f93aafb911
-size 1770
+oid sha256:2b236f404f2767d8e848fb805a62e9c391f4e7a7acb43376d2c7407751b0ad04
+size 7901
